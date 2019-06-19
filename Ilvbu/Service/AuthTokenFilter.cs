7ff5d2aea94ded4a74f1f5e3469556618b896595
绍厅 陈 <897884959@qq.com>
2019-06-19 17:59:41 +0800

    服务名称修改

---
+++ Ilvbu/Service/AuthTokenFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient;
+using WebApiClient.Contexts;
+
+namespace Ilvbu.Service
+{
+    public class AuthTokenFilter : IApiActionFilter
+    {
+        private string _tokenName = "Authorization";
+        public string TokenName
+        {
+            get { return _tokenName; }
+            set { _tokenName = value; }
+        }
+
+        private string _tokenString;
+
+        public AuthTokenFilter(string tokenString)
+        {
+            _tokenString = tokenString;
+        }
+
+        public Task OnBeginRequestAsync(ApiActionContext context)
+        {
+            if (!string.IsNullOrEmpty(_tokenString))
+            {
+                context.RequestMessage.Headers.Add(_tokenName, _tokenString);
+            }
+            return Task.FromResult<object>(null);
+        }
+
+        public Task OnEndRequestAsync(ApiActionContext context)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ Ilvbu/Service/WxController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ilvbu.Service
+{
+    public class WxController :Controller
+    {
+    }
+}
